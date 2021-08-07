2ce52af45cad51f52edc8e7d14316bcd2945b824
samlekebab <84096622+samlekebab@users.noreply.github.com>
2021-08-07 11:23:20 +0200

    refactor of icon for qrcode scanner view (api19 compatible)

--- app/src/main/java/com/atheris/qrcodepass/MCaptureActivity.kt
+++ app/src/main/java/com/atheris/qrcodepass/MCaptureActivity.kt
@@ -2,6 +2,7 @@ package com.atheris.qrcodepass
 
 import android.app.ActionBar
 import android.content.Context
+import android.content.res.Resources
 import android.graphics.BitmapFactory
 import android.hardware.camera2.CameraManager
 import android.os.Bundle
@@ -12,6 +13,8 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.Toast
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.core.content.ContextCompat
 import com.journeyapps.barcodescanner.CaptureActivity
 import com.atheris.qrcodepass.qrcode.logd
 
@@ -19,16 +22,20 @@ class MCaptureActivity : CaptureActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //setTheme(R.style.Theme_PasseSanitaire)
         addContentView(layoutInflater.inflate(R.layout.top_of_scanner,null), ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.MATCH_PARENT))
         with(findViewById<ImageButton>(R.id.torch)) {
-            setImageResource(R.drawable.ic_baseline_flash_on_24)
+            setImageResource(R.drawable.ic_flash)
+            setBackgroundColor(ContextCompat.getColor(context,R.color.back))
             setOnClickListener(){
                 setResult(MainActivity.ResultCodes.QR_TORCH_TOGGLE.ordinal)
                 finish()
             }
         }
         with(findViewById<ImageButton>(R.id.qr_file)){
-            setImageResource(R.drawable.ic_baseline_folder_24)
+            setImageResource(R.drawable.ic_folder)
+            setBackgroundColor(ContextCompat.getColor(context,R.color.back))
+            alpha=1f
             setOnClickListener(){
                 setResult(MainActivity.ResultCodes.QR_FILE_PICK.ordinal)
                 finish()
--- qrcode/src/main/java/com/atheris/qrcodepass/qrcode/QrGetter.kt
+++ qrcode/src/main/java/com/atheris/qrcodepass/qrcode/QrGetter.kt
@@ -5,9 +5,11 @@ import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.util.Log
 import android.widget.ImageView
+import android.widget.Toast
 import com.google.zxing.BarcodeFormat
 import com.google.zxing.datamatrix.DataMatrixWriter
 import com.google.zxing.qrcode.QRCodeWriter
+import java.lang.IllegalArgumentException
 import java.net.URLDecoder
 
 var logd = {msg:String-> Log.d("mtag",msg)}
@@ -30,13 +32,13 @@ open class QrGetter(val context: Context, val sharedPrefName : String,val width:
     }
     fun getQr():Bitmap{
         if (!isInit){
-            logd("not init")
+            //logd("not init")
             val qrCodeString = context.getSharedPreferences(sharedPrefName, Context.MODE_PRIVATE).getString("qrCode","")!!
-            logd("post shared pref")
-            logd(qrCodeString)
+            //logd("post shared pref")
+            //logd(qrCodeString)
             if (!qrCodeString.isNullOrEmpty()) {
                 updateQr(qrCodeString.toString())
-                logd("update succeed")
+                //logd("update succeed")
                 isInit = true
                 return mImage
             }
@@ -67,28 +69,37 @@ open class QrGetter(val context: Context, val sharedPrefName : String,val width:
         }
         mImage = Bitmap.createBitmap(width, width, Bitmap.Config.RGB_565)
         //Log.d("mtag",value);
-        if (!value.isNullOrEmpty()) {
-            val qrCodeWriter = if (isEU()){
-                QRCodeWriter()
-            }else{
-                DataMatrixWriter()
-            }
-            val bitMatrix = qrCodeWriter.encode(
-                value,
-                if (isEU()) {
-                    BarcodeFormat.QR_CODE}else{BarcodeFormat.DATA_MATRIX},
-                width, width
-            )
-
-            for (i in 0 until width) {
-                for (j in 0 until width) {
-                    mImage.setPixel(i, j, if (bitMatrix[i, j]) 0 else 0xFFFFFF)
-
+        try {
+            if (!value.isNullOrEmpty()) {
+                val qrCodeWriter = if (isEU()) {
+                    QRCodeWriter()
+                } else {
+                    DataMatrixWriter()
                 }
-            }
+                val bitMatrix = qrCodeWriter.encode(
+                    value,
+                    if (isEU()) {
+                        BarcodeFormat.QR_CODE
+                    } else {
+                        BarcodeFormat.DATA_MATRIX
+                    },
+                    width, width
+                )
 
-        }else{
-            isInit =false;
+                for (i in 0 until width) {
+                    for (j in 0 until width) {
+                        mImage.setPixel(i, j, if (bitMatrix[i, j]) 0 else 0xFFFFFF)
+
+                    }
+                }
+
+            } else {
+                isInit = false;
+            }
+        }catch (e:IllegalArgumentException){
+            isInit=false
+            e.printStackTrace()
+            Toast.makeText(context,"I'm sorry, exception raised, bad qrcode \n $e",Toast.LENGTH_LONG).show()
         }
         //request a widget update
         widgetUpdate()
@@ -99,7 +110,7 @@ open class QrGetter(val context: Context, val sharedPrefName : String,val width:
         if (start.length>=4) {
             start = start.slice(0..3)
             //logd("$start")
-            if (start == "HC1:" || start == "HC2:") {
+            if (start == "HC1:" || start == "HC2:" || start == "HC3:" || start == "HC4:" ) {
                 return true
             }
         }
